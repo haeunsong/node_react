3fc0db48c7d9d687b9e30e1023b93be445a74283
haeunsong <jamy0225@naver.com>
2021-01-31 21:54:15 +0900

    로그아웃 페이지 + 철자오류 수정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,18 +1,35 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
 
   useEffect(()=>{
     axios.get('/api/hello')
     .then(resp=>console.log(resp))
   },[])
+  
+  const onClickHandler = () => {
+    axios.get('/api/logout')
+    .then(resp=>{
+      if(resp.data.success){
+        props.history.push('/login')
+      }else{
+        alert('로그아웃에 실패했습니다.')
+      }
+      console.log(resp.data)
+    })
+
+  }
 
   return (
     <div style={{display:'flex', justifyContent:'center',alignItems:'center'
       ,width:'100%',height:'100vh'
     }}>
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   );
 };
